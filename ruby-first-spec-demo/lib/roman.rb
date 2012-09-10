c261951b3767d822e9928f7c8c8e4a7863a4551e
Daniel Yshii <andulink@gmail.com>
2012-09-09 22:31:55 -0500

    Cuarto test exitoso

--- ruby-first-spec-demo/lib/roman.rb
+++ ruby-first-spec-demo/lib/roman.rb
@@ -1,11 +1,14 @@
 class Roman
   def convert(veces)
   		unidad = "I"
+  		cinco = "V"
   		retorno = ""
 
 		veces.times do |i|
 			if i < 3
 				retorno = "#{retorno}#{unidad}"
+			elsif i ==3
+				retorno = "#{unidad}#{cinco}"
 			end
 		end
 		retorno
