762dce2d1d319bc07522015889cf60efc5658bf1
Daniel Yshii <andulink@gmail.com>
2012-09-09 23:44:30 -0500

    Vigesimo test exitoso

--- ruby-first-spec-demo/lib/roman.rb
+++ ruby-first-spec-demo/lib/roman.rb
@@ -1,36 +1,31 @@
 class Roman
-  def convert(veces)
+  def convert(veces, acumulado="")
   		unidad = "I"
-  		cinco = "V"
+  		cinco= "V"
   		diez  = "X"
   		retorno = ""
+
+  		if(acumulado != "")
+  			retorno = acumulado
+  		end
 
 		veces.times do |i|
 			if i < 3
 				retorno = "#{retorno}#{unidad}"
 			elsif i ==3
-				retorno = "#{unidad}#{cinco}"
+				retorno = "#{acumulado}#{unidad}#{cinco}"
 			elsif i == 4
-				retorno = "#{cinco}"
+				retorno = "#{acumulado}#{cinco}"
 			elsif i<8
 				retorno = "#{retorno}#{unidad}"
 			elsif i==8
-				retorno = "#{unidad}#{diez}"
+				retorno = "#{acumulado}#{unidad}#{diez}"
 			elsif i==9
-				retorno	= "#{diez}"
-			elsif i == 10
-				retorno	= "#{diez}#{unidad}"
-			elsif i < 13
-				retorno	= "#{retorno}#{unidad}"
-			elsif i == 13
-				retorno	= "#{diez}#{unidad}#{cinco}"
-			elsif i == 14
-				retorno = "#{diez}#{cinco}"
-			elsif i < 18
-				retorno = "#{retorno}#{unidad}"
+				retorno	= "#{acumulado}#{diez}"					
 			else
-				retorno = "#{diez}#{unidad}#{diez}"
+				retorno = convert(veces-10,diez)
 			end
+
 		end
 		retorno
 		
